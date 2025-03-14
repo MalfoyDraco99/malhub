ea7d1e9fa18adf6204d9470a26d5b5ce9e9c7e91
MalfoyDraco99 <owenwverge@gmail.com>
2025-03-14 00:20:27 -0300

    Update and rename malhub.lua to Malhub.lua

---
+++ Malhub.lua
@@ -0,0 +1,4 @@
+local Players = game:GetService("Players")
+local player = Players.LocalPlayer
+
+player:Kick("Malhub has moved to a different loadstring and keysystem, please get a free key with a linkvertise at https://discord.gg/PVQrNa4M7x or buy a key at https://malhub.bgng.io/")
--- malhub.lua
+++
@@ -1,4 +0,0 @@
-local Players = game:GetService("Players")
-local player = Players.LocalPlayer
-
-player:Kick("Malhub has moved to a different loadstring and keysystem, please get a free key with a linkvertise at https://discord.gg/PVQrNa4M7x or buy a key at https://malhub.bgng.io/")
